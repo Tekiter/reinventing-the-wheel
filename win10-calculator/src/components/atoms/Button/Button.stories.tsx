cf97949499e99f44e332c16aebc8bd1cf0d43c65
Tekiter <tekiter8@gmail.com>
2021-02-14 22:16:20 +0900

    Create button component

--- win10-calculator/src/components/atoms/Button/Button.stories.tsx
+++ win10-calculator/src/components/atoms/Button/Button.stories.tsx
@@ -13,7 +13,18 @@ export default {
 
 const Template: Story<ButtonProps> = (args) => <Button {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {
-    children: "Primary"
+export const Number = Template.bind({});
+Number.args = {
+    children: "5",
+    color: "#ffffff",
+    fontSize: "1.3rem",
+    bold: true
+};
+
+export const Util = Template.bind({});
+Util.args = {
+    children: "M+",
+    color: "#f7f7f7",
+    dense: true,
+    bold: true
 };
--- win10-calculator/src/components/atoms/Button/index.tsx
+++ win10-calculator/src/components/atoms/Button/index.tsx
@@ -1,22 +1,40 @@
 import styled from "styled-components";
-import { darken } from "polished";
+import { darken, readableColor } from "polished";
 
 export interface ButtonProps {
     children: React.ReactNode;
-    color: string;
+    color?: string;
+    dense?: boolean;
+    bold?: boolean;
+    fontSize: number | string;
 }
 
-const ButtonBase = styled.button<ButtonProps>`
+interface ButtonBaseProps {
+    color: string;
+    dense: boolean;
+    bold: boolean;
+    fontSize: number | string;
+}
+
+const ButtonBase = styled.button<ButtonBaseProps>`
     display: block;
 
-    background-color: ${(props) => props.color};
-    border: 1px solid ${(props) => props.color};
+    background-color: ${({ color }) => color};
+    border: 1px solid ${({ color }) => color};
+    color: ${({ color }) => readableColor(color)};
+
+    padding: ${({ dense }) => (dense ? "0.2em 0.5em" : "0.5em 1em")};
+
+    font-weight: ${({ bold }) => (bold ? "bold" : "normal")};
+    font-size: ${({ fontSize }) => fontSize};
 
     &:hover {
-        border: 1px solid ${(props) => darken(0.1, props.color)};
+        background-color: ${({ color }) => darken(0.1, color)};
+        border: 1px solid ${({ color }) => darken(0.2, color)};
     }
 
     &:active {
+        background-color: ${({ color }) => darken(0.2, color)};
     }
 
     &:focus {
@@ -25,6 +43,10 @@ const ButtonBase = styled.button<ButtonProps>`
 `;
 
 export default (function Button(props) {
-    const { children, color = "#f2f2f2" } = props;
-    return <ButtonBase color={color}>{children}</ButtonBase>;
+    const { children, color = "#f2f2f2", dense = false, bold = false, fontSize = "" } = props;
+    return (
+        <ButtonBase color={color} dense={dense} bold={bold} fontSize={fontSize}>
+            {children}
+        </ButtonBase>
+    );
 } as React.FC<ButtonProps>);
