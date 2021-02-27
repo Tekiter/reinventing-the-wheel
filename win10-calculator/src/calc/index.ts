1dfeaeca616206503bbf8398cb0fb3fc7f9a82e6
Tekiter <tekiter8@gmail.com>
2021-02-27 22:29:07 +0900

    Add real number input feature

--- win10-calculator/src/calc/index.ts
+++ win10-calculator/src/calc/index.ts
@@ -40,17 +40,41 @@ function actionState(state: CalcState, code: KeyCode): CalcState {
             if (primary.length === 0) {
                 primary = "0";
             }
+
+            if (primary === "-0" || primary === "-") {
+                primary = "0";
+            }
             return { ...state, primary };
         }
     } else if (action === "CE") {
         return { ...state, primary: "0" };
+    } else if (action === "C") {
+        return { ...state, primary: "0" };
+    }
+    return { ...state };
+}
+
+function realState(state: CalcState, code: KeyCode): CalcState {
+    const action = codeSuffix(code);
+
+    if (action === ".") {
+        if (state.primary !== "0" && state.primary.indexOf(".") == -1) {
+            return { ...state, primary: state.primary + "." };
+        }
+    } else if (action === "+-") {
+        if (state.primary[0] === "-") {
+            return { ...state, primary: state.primary.substring(1) };
+        } else if (state.primary !== "0") {
+            return { ...state, primary: "-" + state.primary };
+        }
     }
     return { ...state };
 }
 
 const stateMap: { [type: string]: (state: CalcState, code: KeyCode) => CalcState } = {
     NUM: numberState,
-    ACN: actionState
+    ACN: actionState,
+    REA: realState
 };
 
 function key(state: CalcState, code: KeyCode): CalcState {
